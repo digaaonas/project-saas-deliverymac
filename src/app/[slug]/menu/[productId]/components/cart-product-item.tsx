b204057ed23e1633f9aada0e14339b25404e921a
Diego Nascimento <digaaonas@gmail.com>
2025-02-27 19:24:49 -0300

    feat: ensure cant product can be removed

--- src/app/[slug]/menu/[productId]/components/cart-product-item.tsx
+++ src/app/[slug]/menu/[productId]/components/cart-product-item.tsx
@@ -12,7 +12,7 @@ interface CartProductItemProps {
 }
 
 const CartProductItem = ({product}: CartProductItemProps) => {
-  const {decreaseProductQuantity, increaseProductQuantity} = useContext(CartContext)
+  const {decreaseProductQuantity, increaseProductQuantity, removeProduct} = useContext(CartContext)
   return ( 
    <div className="flex items-center justify-between">
     {/*LADO ESQUERDO*/}
@@ -36,7 +36,7 @@ const CartProductItem = ({product}: CartProductItemProps) => {
         </div>
       </div>
     </div>
-    <Button variant="outline" className="h-7 w-7 rounded-lg">
+    <Button variant="outline" className="h-7 w-7 rounded-lg" onClick={() => removeProduct(product.id)}>
       <TrashIcon/>
     </Button>
    </div> 
