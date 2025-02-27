f3a2f10255b32cb5297ff63b2add7cff25c6b40e
Diego Nascimento <digaaonas@gmail.com>
2025-02-27 19:02:17 -0300

    feat: add initial cart item component (without functionality)

---
+++ src/app/[slug]/menu/[productId]/components/cart-product-item.tsx
@@ -0,0 +1,44 @@
+import { ChevronLeftIcon, ChevronRightIcon, TrashIcon } from "lucide-react";
+import Image from "next/image";
+
+import { Button } from "@/components/ui/button";
+import { formatCurrency } from "@/helpers/format-currency";
+
+import { CartProduct } from "../../contexts/cart";
+
+interface CartProductItemProps {
+  product: CartProduct
+}
+
+const CartProductItem = ({product}: CartProductItemProps) => {
+  return ( 
+   <div className="flex items-center justify-between">
+    {/*LADO ESQUERDO*/}
+    <div className="flex items-center gap-3">
+      <div className="relative h-20 w-20 rounded-xl bg-gray-100">
+        <Image src={product.imageUrl} alt={product.name} fill/>
+      </div>
+      <div className="space-y-1">
+        <p className="text-xs max-w-[90%] truncate text-ellipsis">{product.name}</p>
+        <p className="text-sm font-semibold">{formatCurrency(product.price)}</p>
+
+        {/*BOTÕES E QUANTTIDADE*/}
+        <div className="flex items-center gap-1 text-center">
+          <Button variant="outline" className="h-7 w-7 rounded-lg">
+            <ChevronLeftIcon/>
+          </Button>
+          <p className="w-7 text-xs">{product.quantity}</p>
+          <Button variant="destructive" className="h-7 w-7 rounded-lg">
+            <ChevronRightIcon/>
+          </Button>
+        </div>
+      </div>
+    </div>
+    <Button variant="outline" className="h-7 w-7 rounded-lg">
+      <TrashIcon/>
+    </Button>
+   </div> 
+   );
+}
+ 
+export default CartProductItem;
--- src/app/[slug]/menu/[productId]/components/cart-sheet.tsx
+++ src/app/[slug]/menu/[productId]/components/cart-sheet.tsx
@@ -1,27 +1,43 @@
-import { useContext } from "react";
+import { ChevronLeftIcon, ChevronRightIcon, Trash2 } from "lucide-react";
+import Image from "next/image";
+import { useContext, useState } from "react";
 
-import { Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle } from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
+import { Sheet, SheetContent, SheetFooter, SheetHeader, SheetTitle } from "@/components/ui/sheet";
+import { formatCurrency } from "@/helpers/format-currency";
 
 import { CartContext } from "../../contexts/cart";
+import CartProductItem from "./cart-product-item";
 
 const CartSheet = () => {
     const {isOpen, toggleCart, products} = useContext(CartContext)
+    const [quantity, setQuantity] = useState<number>(1)
+      const handleDecreaseQuantity = () => {
+        setQuantity((prev) => {
+          if (prev == 1){
+            return 1
+          }
+          return prev - 1
+        })
+      }
+      const handleIncreaseQuantity = () => {
+        
+        setQuantity((prev) => prev + 1)
+        
+        }
   return ( 
     <Sheet open={isOpen} onOpenChange={toggleCart}>
-        <SheetContent>
-          <SheetHeader>
-            <SheetTitle>Are you absolutely sure?</SheetTitle>
-            <SheetDescription>
-              This action cannot be undone. This will permanently delete your account
-              and remove your data from our servers.
-            </SheetDescription>
+        <SheetContent className="w-[85%]">
+          <SheetHeader className="relative">
+            <SheetTitle className="flex left-0 mb-5">Sacola</SheetTitle>
           </SheetHeader>
-          {products.map(product => (
-            <h1 key={product.id}>{product.name} - {product.quantity}</h1>
-          ))}
+            <div className="py-5">
+              {products.map(product => (
+                <CartProductItem key={product.id} product={product}/>
+              ))}
+            </div>
         </SheetContent>
       </Sheet>
    );
 }
- 
 export default CartSheet;
